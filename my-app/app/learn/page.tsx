8b8756bddcb4eab4d9862f741b9b3fb2a100ebd0
RioPutra777 <alohario2004@gmail.com>
2026-04-30 12:30:30 +0800

    lanjut

---
+++ my-app/app/learn/page.tsx
@@ -0,0 +1,14 @@
+export default function learnpge() {
+  return (
+    <div className="bg-pink-500">
+      <div className='bg-orange-300'>
+        <img/>
+        <p>Design</p>
+      </div>
+      <div>
+        <h1>FIGMA</h1>
+        <p>8 hours ago</p>
+      </div>
+    </div>
+  )
+}
